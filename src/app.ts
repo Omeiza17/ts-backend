4415421d0d74b0781a60d146b6de8161c8be47cd
Michael Omeiza Suleiman <mosuleiman19@gmail.com>
2020-11-18 16:33:47 +0400

    set-up completed

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,8 @@
-const greeting: string = 'Hello';
-const numbers: number[] = [1, 2, 3];
+import express, {Application, Request, Response, NextFunction} from 'express';
+
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send({status: 'UP'});
+});
+app.listen(3000, () => console.log('Server is running'));
